0c685bcb6db4aeca3ab678201904434b532f5452
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-02-27 22:33:11 +0600

    added modal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-const loadPhone = async(search) =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
-    const data = await res.json()
-    const phone = data.data
-    displayPhone(phone)
+const loadPhone = async (search, isShowAll) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
+  const data = await res.json()
+  const phone = data.data
+  displayPhone(phone, isShowAll)
 }
 
-const displayPhone = phones =>{
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.textContent = '';
-phones.forEach(phone => {
+const displayPhone = (phones, isShowAll) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.textContent = '';
+// if items 12 more show the show all btn and less than hidden
+const showAllContainer = document.getElementById("showAll-container");
+if(phones.length > 12 && !isShowAll){
+  showAllContainer.classList.remove("hidden");
+}
+else{
+  showAllContainer.classList.add("hidden");
+}
+
+  // many item show output i can slice and show some specific items and if not show all
+  // phones = phones.slice(0, 12);
+  if(!isShowAll){
+    phones = phones.slice(0, 12);
+  }
+
+
+  phones.forEach(phone => {
     console.log(phone)
     const div = document.createElement("div");
     div.innerHTML = `
@@ -17,21 +33,49 @@ phones.forEach(phone => {
     <div class="card-body">
       <h2 class="card-title">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+      <div class="card-actions justify-center">
+        <button onclick="handleShowDetails('${phone.slug}'); show_details_modal.showModal()" class="btn btn-primary">Show Details</button>
       </div>
     </div>
   </div>
     `
     cardContainer.appendChild(div);
-    
-});
+
+  });
+  // set loading close
+  toggleLoadingSpinner(false);
+}
+// show details modal
+const handleShowDetails = async(id) =>{
+  // console.log("yes i am click", id)
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  console.log(data);
 }
 
-const handleSearch = () =>{
-    const inputField = document.getElementById("input-field");
-    const searchField = inputField.value; 
-    loadPhone(searchField);
+
+// handle search input
+const handleSearch = (isShowAll) => {
+  toggleLoadingSpinner(true);
+  const inputField = document.getElementById("input-field");
+  const searchField = inputField.value;
+  loadPhone(searchField, isShowAll);
+}
+
+// set loading spinner
+const toggleLoadingSpinner = (isLoading) =>{
+  const loadingSpinner = document.getElementById("loading-spinner");
+if(isLoading){
+  loadingSpinner.classList.remove("hidden");
+}
+else{
+  loadingSpinner.classList.add("hidden");
+}
+}
+
+// show all btn
+const toggleShowALl = () =>{
+  handleSearch(true);
 }
 
 // loadPhone();
