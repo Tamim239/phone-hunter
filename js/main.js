a0e09c0e1640a6fd8954173d4cc491fd80da179b
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-02-28 12:28:22 +0600

    added dynamic modal and seacrh and show details

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const loadPhone = async (search, isShowAll) => {
+const loadPhone = async (search = 'a', isShowAll) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
   const data = await res.json()
   const phone = data.data
@@ -7,25 +7,26 @@ const loadPhone = async (search, isShowAll) => {
 
 const displayPhone = (phones, isShowAll) => {
   const cardContainer = document.getElementById("card-container");
+  // this text content '' means clear old content empty
   cardContainer.textContent = '';
-// if items 12 more show the show all btn and less than hidden
-const showAllContainer = document.getElementById("showAll-container");
-if(phones.length > 12 && !isShowAll){
-  showAllContainer.classList.remove("hidden");
-}
-else{
-  showAllContainer.classList.add("hidden");
-}
+  // if items 12 more show the show all btn and less than hidden
+  const showAllContainer = document.getElementById("showAll-container");
+  if (phones.length > 12 && !isShowAll) {
+    showAllContainer.classList.remove("hidden");
+  }
+  else {
+    showAllContainer.classList.add("hidden");
+  }
 
   // many item show output i can slice and show some specific items and if not show all
   // phones = phones.slice(0, 12);
-  if(!isShowAll){
+  if (!isShowAll) {
     phones = phones.slice(0, 12);
   }
 
 
   phones.forEach(phone => {
-    console.log(phone)
+    // console.log(phone)
     const div = document.createElement("div");
     div.innerHTML = `
     <div class="card w-96 bg-base-100 p-4 shadow-xl">
@@ -45,14 +46,43 @@ else{
   // set loading close
   toggleLoadingSpinner(false);
 }
+
+
 // show details modal
-const handleShowDetails = async(id) =>{
+const handleShowDetails = async (id) => {
   // console.log("yes i am click", id)
   const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
   const data = await res.json();
-  console.log(data);
+  const phone = data.data;
+  // console.log(phone);
+  showPhoneDetails(phone);
 }
 
+// show details btn dynamic load after load modal
+const showPhoneDetails = (phone) => {
+  // console.log(phone)
+  const phoneImage = document.getElementById("show-details-image");
+  phoneImage.innerHTML = `
+<img src="${phone.image}" alt="">
+`
+  const phoneName = document.getElementById("show-detail-phoneName");
+  phoneName.innerText = phone.name;
+
+  const showDetailsContainer = document.getElementById("show-details-container");
+  showDetailsContainer.innerHTML = `
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+<p>Storage : ${phone?.mainFeatures?.storage}</p>
+<p>Display Size : ${phone?.mainFeatures?.displaySize} </p>
+<p>ChipSet : ${phone?.mainFeatures?.chipSet}</p>
+<p>Memory : ${phone?.mainFeatures?.memory}</p>
+<p>Slug : ${phone?.slug}</p>
+<p>Release data : ${phone?.releaseDate}</p>
+<p>Brand : ${phone?.brand}</p>
+<p>GPS : ${phone?.others?.GPS || 'No GPS available'}</p>
+`
+
+  show_details_modal.showModal();
+}
 
 // handle search input
 const handleSearch = (isShowAll) => {
@@ -63,19 +93,19 @@ const handleSearch = (isShowAll) => {
 }
 
 // set loading spinner
-const toggleLoadingSpinner = (isLoading) =>{
+const toggleLoadingSpinner = (isLoading) => {
   const loadingSpinner = document.getElementById("loading-spinner");
-if(isLoading){
-  loadingSpinner.classList.remove("hidden");
-}
-else{
-  loadingSpinner.classList.add("hidden");
-}
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  }
+  else {
+    loadingSpinner.classList.add("hidden");
+  }
 }
 
 // show all btn
-const toggleShowALl = () =>{
+const toggleShowALl = () => {
   handleSearch(true);
 }
 
-// loadPhone();
+loadPhone();
